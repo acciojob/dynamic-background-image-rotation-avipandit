461d9a98c213e7cb1cc750f046afd9ca0d62957d
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-06-13 20:01:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+function changeBackgroundImage() {
+  var backgroundImageURL;
+
+  if (window.innerWidth > window.innerHeight) {
+    // Landscape mode
+    backgroundImageURL = 'https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg';
+  } else {
+    // Portrait mode
+    backgroundImageURL = 'https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg';
+  }
+  document.body.style.backgroundImage = 'url(' + backgroundImageURL + ')';
+}
+window.addEventListener('resize', changeBackgroundImage);
+
+changeBackgroundImage();
